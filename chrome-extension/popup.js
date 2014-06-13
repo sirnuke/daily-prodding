1266a348d0b24e572ff66cdfa315d817262890b0
Bryan DeGrendel <bryan@degrendel.com>
2014-06-13 16:00:31 -0400

    Added a few bits of structure

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,1 +1,12 @@
 //
+
+var duolingo = {
+  check: function() {
+  }
+};
+
+//window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("onload " + Date())
+});
+
