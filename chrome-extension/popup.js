f73e8172a5afe96aa0242475fbaa6ea36a772287
Bryan DeGrendel <bryan@degrendel.com>
2014-06-18 18:42:11 -0400

    UI improvements, plus added GitHub checking

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -15,10 +15,38 @@ var duolingo = {
   }
 };
 
-//window.onload = function() {
+var github = {
+  today: function() {
+    var now = new Date(Date.now());
+    var month = now.getMonth() + 1;
+    var day = now.getDate();
+    var result = now.getFullYear() + "/";
+    if (month < 10)
+      result += "0";
+    result += month + "/";
+    if (day < 10)
+      result += "0";
+    result += day;
+    console.log(result);
+    return result;
+  },
+
+  check: function(result) {
+    //var today = github.today();
+    var last = result.length - 1;
+    //if (result[last][0] != today)
+    //  if it doesn't match, we have a date mismatch of some sort
+    if (result[last].length != 2 || typeof result[last][1] != "number")
+      $("#github").html("GitHub: Unexpected latest data format");
+    else if (result[last][1] > 0)
+      $("#github").html("GitHub: Good!");
+    else
+      $("#github").html("GitHub: Code today!");
+  }
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   $.get("https://www.duolingo.com/", duolingo.check);
-
-  console.log("onload " + Date());
+  $.get("https://github.com/users/sirnuke/contributions_calendar_data", github.check);
 });
 
