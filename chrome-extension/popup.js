f53be5c91f2373cda7bf657aebbcb81fba530f72
Bryan DeGrendel <bryan@degrendel.com>
2014-06-17 23:49:12 -0400

    Partially checks Duolingo results

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,9 +1,17 @@
 //
 
 var duolingo = {
+  regex: /"streak_extended_today"\s*:\s*(\w+)\s*,/,
   check: function(result) {
-    alert(result);
-    $("#duolingo").html("got duolingo!");
+    var check = duolingo.regex.exec(result);
+    if (check == null)
+      $("#duolingo").html("Duoling: logged in?");
+    else if (check[1] == 'true')
+      $("#duolingo").html("Duolingo: Good!");
+    else if (check[1] == 'false')
+      $("#duolingo").html("Duolingo: Practice today!");
+    else
+      $("#duolingo").html("Duolingo: Unknown value '" + check[1] + "'");
   }
 };
 
